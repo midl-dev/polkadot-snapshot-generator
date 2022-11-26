ed3850bac9ee20a5f9fb80d8ebfc6af63ec0679f
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-11-25 16:06:16 -0800

    handle the case where there is nothing to delete

--- docker/snapshot-uploader/filterOldSnapshots.py
+++ docker/snapshot-uploader/filterOldSnapshots.py
@@ -4,4 +4,5 @@ data = json.load(sys.stdin)
 
 objects_to_delete = [ s["Key"] for s in data[:-2] ]
 
-print("\n".join(objects_to_delete))
+if objects_to_delete:
+    print("\n".join(objects_to_delete))
